904ddd8af4b06440618d5ba754fa2b8a5f039b84
trane <xq1281459782@gmail.com>
2021-11-15 16:19:01 +0800

    feat: 配置redis环境

---
+++ application-app/src/main/java/com/lecture/controller/CourseController.java
@@ -0,0 +1,15 @@
+package com.lecture.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @className: CourseController
+ * @description: TODO
+ * @author: carl
+ * @date: 2021/11/15 16:11
+ */
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+}
