3f0ac1b6a2da3e90939f61adcbaae167318922ad
trane <xq1281459782@gmail.com>
2021-11-16 01:20:09 +0800

    feat: 配置调整结构

--- application-app/src/main/java/com/lecture/interceptor/LoginInterceptor.java
+++ application-app/src/main/java/com/lecture/interceptor/LoginInterceptor.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * @className: LoginInterceptor
- * @description: TODO
+ * @description: 登录拦截器
  * @author: carl
  * @date: 2021/11/15 18:45
  */
@@ -25,7 +25,7 @@ public class LoginInterceptor implements HandlerInterceptor {
      * 是否进行拦截
      */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         // 判断是否需要登录
         if (handler instanceof HandlerMethod) {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
@@ -40,14 +40,14 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     public boolean loginJudge(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("application/json;charset=UTF-8");
-        String token = null;
+        String token;
         try {
             token = request.getHeader("token");
+            if (DataUtils.isAnyIsEmpty(token, userService.getByToken(token, response))) {
+                return false;
+            }
         } catch (NullPointerException e) {
             e.printStackTrace();
-        }
-        if (DataUtils.isEmpty(token) || DataUtils.isEmpty(userService.getByToken(token, response))) {
-            return false;
         }
         return true;
     }
---
+++ application-infrastructure/src/main/java/com/lecture/eneities/UserDO.java
@@ -0,0 +1,10 @@
+package com.lecture;
+
+/**
+ * @className: UserDO
+ * @description: TODO
+ * @author: carl
+ * @date: 2021/11/16 0:41
+ */
+public class UserDO {
+}
