3f76b80a9b1c286ab4c43a9ebac3d2a575dcf731
trane <xq1281459782@gmail.com>
2021-11-26 14:59:54 +0800

    解决临时跨域问题

--- application-app/src/main/java/com/lecture/app/config/WebMvcConfig.java
+++ application-app/src/main/java/com/lecture/app/config/WebMvcConfig.java
@@ -18,7 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").
                 //allowedOrigins("https://www.dustyblog.cn"). //允许跨域的域名，可以用*表示允许任何域名使用
-                        allowedOrigins("*").
+                        allowedOrigins("/*").
                 allowedMethods("GET", "POST"). //允许任何方法（post、get等）
                 allowedHeaders("*"). //允许任何请求头
                 allowCredentials(true). //带上cookie信息
