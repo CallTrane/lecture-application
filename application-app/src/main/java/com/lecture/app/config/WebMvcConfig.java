f3fb5e8f8d0eb90e75c6dd5cd8bfc98bf9ed5e6f
trane <xq1281459782@gmail.com>
2021-11-26 14:57:19 +0800

    解决临时跨域问题

---
+++ application-app/src/main/java/com/lecture/app/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.lecture.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @className: CorsConfig
+ * @description: TODO
+ * @author: Carl Tong
+ * @date: 2021/11/26 14:42
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                //allowedOrigins("https://www.dustyblog.cn"). //允许跨域的域名，可以用*表示允许任何域名使用
+                        allowedOrigins("*").
+                allowedMethods("GET", "POST"). //允许任何方法（post、get等）
+                allowedHeaders("*"). //允许任何请求头
+                allowCredentials(true). //带上cookie信息
+                exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(3600L); //maxAge(3600)表明在3600秒内，不需要再发送预检验请求，可以缓存该结果
+    }
+}
