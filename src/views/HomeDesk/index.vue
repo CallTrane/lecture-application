f8dff02db5a218eed0e5c471034418168caf4a7a
CavisGazer <1150896081@qq.com>
2021-12-03 00:09:29 +0800

    背景图添加

--- src/views/HomeDesk/index.vue
+++ src/views/HomeDesk/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
-    <el-container class="header">
-      <el-header>Header</el-header>
-    </el-container>
-    <el-container class="middle">
-      <el-aside>Aside</el-aside>
-      <el-main>Main</el-main>
-    </el-container>
-    <el-container class="bottom">
-      <el-footer>Footer</el-footer>
-    </el-container>
+    <div class="content">
+      <el-container class="header">
+        <el-header>学生选课系统</el-header>
+      </el-container>
+      <el-container class="middle">
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+      <el-container class="bottom">
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </div>
   </div>
 </template>
 
@@ -23,21 +25,49 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  /* background-image: url("../../assets/img/background.jpg");
+  background-size: cover;
+  background-attachment: fixed; */
+  position: relative;
+  /* filter: blur(5px); */
+}
+.container::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-image: url("../../assets/img/background2.jpg");
+  background-size: cover;
+  filter: blur(5px);
+}
+.content {
+  position: relative;
+  z-index: 1;
+}
 .header {
-  height: 70px;
-  background-color: skyblue;
+  height: 68px;
+  line-height: 68px;
+  font-size: 32px;
+  letter-spacing: 1px;
+  font-family: Arial;
+  color: #333;
+  text-align: center;
+  font-weight: 600;
 }
 .bottom {
   position: relative;
-  height: 66px;
-  top: calc(100vh- (100vh - 136px));
-  background-color: skyblue;
+  height: 61px;
+  top: calc(100vh- (100vh - 130px));
+  text-align: center;
 }
 .el-aside,
 .el-main {
-  background-color: pink;
-  border: 1px solid #000;
-  height: calc(100vh - 136px);
+  border: 2px solid #000;
+  height: calc(100vh - 130px);
+  text-align: center;
 }
 .el-main {
   width: 100%;
